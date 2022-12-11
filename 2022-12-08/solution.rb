fc443b5b8c6aa82329d4074a1c3d5038bdd400c1
Bartosz Szar <szarbartosz@gmail.com>
2022-12-11 11:52:26 +0100

    optimize day-8 solution

--- 2022-12-08/solution.rb
+++ 2022-12-08/solution.rb
@@ -1,138 +1,77 @@
-def is_visible_from_top(i, j, forrest)
+def is_visible_from(i, j, direction, forrest)
+  height, width = forrest.length, forrest[i].length;
   tree_height = forrest[i][j];
-  current_y = i - 1;
-  while current_y >= 0
-    if forrest[current_y][j] >= tree_height
-      return false;
-    elsif current_y == 0
-      return true;
-    end
-    current_y -= 1;
-  end
-  return false;
-end
 
-def is_visible_from_bottom(i, j, forrest)
-  height = forrest.length;
-  tree_height = forrest[i][j];
-  current_y = i + 1;
-  while current_y < height
-    if forrest[current_y][j] >= tree_height
-      return false;
-    elsif current_y == height - 1
-      return true;
-    end
-    current_y += 1;
-  end
-  return false;
-end
+  current_y, current_x = i, j;
+  current_y -= 1 if direction == "top";
+  current_y += 1 if direction == "bottom";
+  current_x -= 1 if direction == "left";
+  current_x += 1 if direction == "right";
 
-def is_visible_from_left(i, j, forrest)
-  tree_height = forrest[i][j];
-  current_x = j - 1;
-  while current_x >= 0
-    if forrest[i][current_x] >= tree_height
-      return false;
-    elsif current_x == 0
-      return true;
-    end
-    current_x -= 1;
-  end
-  return false;
-end
+  while (0...height).include?(current_y) && (0...width).include?(current_x)
+    return false if forrest[current_y][current_x] >= tree_height;
+    return true if direction == "top" && current_y.zero?;
+    return true if direction == "bottom" && current_y == height - 1;
+    return true if direction == "left" && current_x.zero?;
+    return true if direction == "right" && current_x == width - 1;
 
-def is_visible_from_right(i, j, forrest)
-  width = forrest[i].length;
-  tree_height = forrest[i][j];
-  current_x = j + 1;
-  while current_x < width
-    if forrest[i][current_x] >= tree_height
-      return false;
-    elsif current_x == width - 1
-      return true;
-    end
-    current_x += 1
+    current_y -= 1 if direction == "top";
+    current_y += 1 if direction == "bottom";
+    current_x -= 1 if direction == "left";
+    current_x += 1 if direction == "right";
   end
-  return false;
+
+  false;
 end
 
 def is_visible(i, j, forrest)
-  return is_visible_from_top(i, j, forrest) || is_visible_from_right(i, j, forrest) || is_visible_from_bottom(i, j, forrest) || is_visible_from_left(i, j, forrest);
+  return is_visible_from(i, j, "top", forrest) || is_visible_from(i, j, "right", forrest) || is_visible_from(i, j, "bottom", forrest) || is_visible_from(i, j, "left", forrest);
 end
 
-def calculate_scenic_score_top(i, j, forrest)
+def calculate_scenic_score_from(i, j, direction, forrest)
+  height, width = forrest.length, forrest[i].length;
   tree_height = forrest[i][j];
-  current_y = i - 1;
-  while current_y >= 0
-    if forrest[current_y][j] >= tree_height
-      return i - current_y;
-    end
-    current_y -= 1;
-  end
-  return i - current_y - 1;
-end
 
-def calculate_scenic_score_bottom(i, j, forrest)
-  height = forrest.length;
-  tree_height = forrest[i][j];
-  current_y = i + 1;
-  while current_y < height
-    if forrest[current_y][j] >= tree_height
-      return current_y - i;
-    end
-    current_y += 1;
-  end
-  return current_y - i - 1;
-end
+  current_y, current_x = i, j;
+  current_y -= 1 if direction == "top";
+  current_y += 1 if direction == "bottom";
+  current_x -= 1 if direction == "left";
+  current_x += 1 if direction == "right";
 
-def calculate_scenic_score_left(i, j, forrest)
-  tree_height = forrest[i][j];
-  current_x = j - 1;
-  while current_x >= 0
-    if forrest[i][current_x] >= tree_height
-      return j - current_x;
+  while (0...height).include?(current_y) && (0...width).include?(current_x)
+    if forrest[current_y][current_x] >= tree_height
+      return i - current_y if direction == "top";
+      return current_y - i if direction == "bottom";
+      return j - current_x if direction == "left";
+      return current_x - j if direction == "right";
     end
-    current_x -= 1;
-  end
-  return j - current_x - 1;
-end
 
-def calculate_scenic_score_right(i, j, forrest)
-  width = forrest[i].length;
-  tree_height = forrest[i][j];
-  current_x = j + 1;
-  while current_x < width
-    if forrest[i][current_x] >= tree_height
-      return current_x - j;
-    end
-    current_x += 1;
+    current_y -= 1 if direction == "top";
+    current_y += 1 if direction == "bottom";
+    current_x -= 1 if direction == "left";
+    current_x += 1 if direction == "right";
   end
-  return current_x - j - 1;
+
+  return i - current_y - 1 if direction == "top";
+  return current_y - i - 1 if direction == "bottom";
+  return j - current_x - 1 if direction == "left";
+  return current_x - j - 1 if direction == "right";
 end
 
 def calculate_scenic_score(i, j, forrest)
-  return calculate_scenic_score_top(i, j, forrest) * calculate_scenic_score_right(i, j, forrest) * calculate_scenic_score_bottom(i, j, forrest) * calculate_scenic_score_left(i, j, forrest);
+  return calculate_scenic_score_from(i, j, "top", forrest) * calculate_scenic_score_from(i, j, "right", forrest) * calculate_scenic_score_from(i, j, "bottom", forrest) * calculate_scenic_score_from(i, j, "left", forrest);
 end
 
 def initialize_forrest()
   width = 0;
   height = 0;
 
-  File.readlines('input.txt').each do |line|
-    width = line.strip.length;
-    height += 1;
-  end
+  lines = File.readlines('input.txt');
+  width = lines.first.strip.length;
+  height = lines.length;
 
-  forrest = Array.new(height){Array.new(width)}
-
-  currentRow = 0;
-
-  File.readlines('input.txt').each do |line|
-    for i in 0 .. width - 1
-      forrest[currentRow][i] = line[i];
-    end
-    currentRow += 1;
-  end
+  forrest = Array.new(height){Array.new(width)};
+  forrest = lines.map { |line| line.strip.chars };
 
   return forrest, width, height;
 end
@@ -144,9 +83,7 @@ def solution_pt_1()
   
   for i in 1 .. height - 2
     for j in 1 .. width - 2
-      if is_visible(i, j, forrest)
-        result += 1;
-      end
+      result += 1 if is_visible(i, j, forrest);
     end
   end
 
