bd32bb2b0f32fda2fa19668737260247cd1dd99c
Bartosz Szar <szarbartosz@gmail.com>
2022-12-11 11:32:38 +0100

    add day-9 pt-2 solution

--- 2022-12-09/solution.pl
+++ 2022-12-09/solution.pl
@@ -3,6 +3,23 @@ sub head_touches_tail {
   return abs($head->[0] - $tail->[0]) <= 1 && abs($head->[1] - $tail->[1]) <= 1;
 }
 
+sub move_head {
+  my ($head, $direction) = @_;
+  if ($direction eq "U") {
+        $head->[1] += 1;
+      }
+      if ($direction eq "R") {
+        $head->[0] += 1;
+      }
+      if ($direction eq "D") {
+        $head->[1] -= 1;
+      }
+      if ($direction eq "L") {
+        $head->[0] -= 1;
+      }
+  return $head;
+}
+
 sub move_tail {
   my ($head, $tail) = @_;
   
@@ -41,45 +58,51 @@ sub move_tail {
   if ($head->[0] < $tail->[0] && $head->[1] > $tail->[1]) {
     $tail->[0] -= 1;
     $tail->[1] += 1;
-
-  return $tail;
   }
+  return $tail;
 }
 
 sub solution_pt_1 {
   open(my $file, "<", "input.txt") or die "Could not open input.txt: $!";
 
   my %result_set;
-  my $tail = [0,0];
-  my $head = [1,2];
 
   while (my $line = <$file>) {
     my ($direction, $steps) = split " ", $line;
     chomp($steps);
     for (my $i = 0; $i < $steps; $i++) {
-      if ($direction eq "U") {
-        $head->[1] += 1;
-        $tail = move_tail($head, $tail);
-      }
-      if ($direction eq "R") {
-        $head->[0] += 1;
-        $tail = move_tail($head, $tail);
-      }
-      if ($direction eq "D") {
-        $head->[1] -= 1;
-        $tail = move_tail($head, $tail);
-      }
-      if ($direction eq "L") {
-        $head->[0] -= 1;
-        $tail = move_tail($head, $tail);
-      }
+      $head = move_head($head, $direction);
+      $tail = move_tail($head, $tail);
       $result_set{join(",", @{$tail})} = 1;
     }
   }
   return scalar(keys %result_set);
 }
 
+sub solution_pt_2 {
+  open(my $file, "<", "input.txt") or die "Could not open input.txt: $!";
+
+  my %result_set;
+  my @rope = map { [0, 0] } (1..10);
+
+  while (my $line = <$file>) {
+    my ($direction, $steps) = split " ", $line;
+    chomp($steps);
+    for (my $i = 0; $i < $steps; $i++) {
+      $rope[0] = move_head($rope[0], $direction);
+
+      for my $i (1 .. $#rope){
+        $rope[$i] = move_tail($rope[$i - 1], $rope[$i]);
+      }
+
+      $result_set{join(",", @{$rope[9]})} = 1;
+    }
+  }
+  return scalar(keys %result_set);
+}
+
 if ($0 eq __FILE__) {
-  print solution_pt_1();
+  print solution_pt_1() . "\n";
+  print solution_pt_2();
 }
  
